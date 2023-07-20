04a866a0455db72dcfb780212a37c342087ec1f1
mannic429 <139977297+mannic429@users.noreply.github.com>
2023-07-20 09:08:55 -0400

    Add files via upload

---
+++ Manni Chen/About Manni Chen/About Manni Chen/ViewController2.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController2.swift
+//  About Manni Chen
+//
+//  Created by SMART Scholars on 7/19/23.
+//
+
+import UIKit
+
+class ViewController2: NSObject {
+
+}
---
+++ Manni Chen/Manni/MiniProject2/MiniProject2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MiniProject2
+//
+//  Created by SMART Scholars on 7/19/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var displayAnwer: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func response1(_ sender: Any) {
+    }
+    
+    @IBAction func response2(_ sender: Any) {
+    }
+    @IBAction func response3(_ sender: Any) {
+    }
+}
+
